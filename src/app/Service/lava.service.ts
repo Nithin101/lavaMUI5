0752c999305749e6c9c08644f7f9719f98a225fa
Nithin <nithinshetty@codecraft.co.in>
2018-01-24 14:36:30 +0530

    service path change

--- src/app/Service/lava.service.ts
+++ src/app/Service/lava.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
-import { MySharedService } from './Service/MySharedService';
+import { MySharedService } from './MySharedService';
 
 
 @Injectable()
